6bc225e7d48d629714cc8bb21dd9c25d86b3fc68
john <bookerboker@gmail.com>
2014-04-14 16:27:30 -0400

    Cleaning up with some formatting

--- ncbookstack_usability/home.php
+++ ncbookstack_usability/home.php
@@ -9,6 +9,7 @@ for removing books from personal lists and changing password atm.
 Possible add-ons: Link to search page, move change password stuff
 to another page.
 -->
+
 <?php
 include 'connect_test.php'; //connect the connection page
 
@@ -22,19 +23,22 @@ if (!isset($_SESSION['username'])) { //if not yet logged in
     header("Location: login.php"); // send to login page
     exit;
 }
-
 ?>
+
 <html>
+
     <head>
         <meta charset="UTF-8">
         <title>NC Bookstack - A used book store for UML students</title>
-        
+
         <link rel="stylesheet" type="text/css" href="css/mainCSS.css">
     </head>
+
     <body>
+        
         <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
         <a href="logout.php">Logout </a>
-	<a href="pw_change_form.php"> Change Password </a> |
+        <a href="pw_change_form.php"> Change Password </a> |
         <a href="search_v2.php"> Search for a book </a><br>
 
         <?php
@@ -55,8 +59,8 @@ if (!isset($_SESSION['username'])) { //if not yet logged in
                 $slice_data = mysql_query("SELECT * FROM bookslice1 WHERE isbn LIKE'%$isbn%'") or die('Error: ' . mysql_error());
                 while ($result_inner = mysql_fetch_array($slice_data)) {
                     //echo '<div class="bookEntry">';
-		    echo '<input type="radio" name="pass_val" value="' . $result_inner['isbn'] . '">';
-		    echo '<div class="bookEntrySelling">';
+                    echo '<input type="radio" name="pass_val" value="' . $result_inner['isbn'] . '">';
+                    echo '<div class="bookEntrySelling">';
                     echo '<strong>Title</strong>: ' . $result_inner['title'];
                     echo '<br>';
                     echo '<strong>Edition</strong>: ' . $result_inner['edition'];
@@ -73,40 +77,46 @@ if (!isset($_SESSION['username'])) { //if not yet logged in
                 }
                 echo '<br>';
             }
-	    echo '</div>';
+            echo '</div>';
+
+            echo '<div class="wantedBooks"><h3><strong>Current Transactions</strong></h3>';
             
-	   echo '<div class="wantedBooks"><h3><strong>Current Transactions</strong></h3>';
-	   $currtran_data = mysql_query("SELECT * FROM current_transactions WHERE seller = '$user' OR buyer = '$user'")or die("CURRTRAN: Error: ".mysql_error());
-	   while ($result = mysql_fetch_array($currtran_data)){
-	     echo '<input type="radio" name="pass_valCT" value="' . $result['isbn'] . '">';
-	     echo '<div class="bookEntry">';
-	     echo '<strong>Trade Date</strong>: '.$result['tradedate'];
-	     echo '<br>';
-	     if($_SESSION['username'] == $result['seller']){
-	      echo '<strong>Buyer</strong>: ' .$result['buyer'];
-	      $other = $result['buyer'];
-	     } 
-	     else if($_SESSION['username'] == $result['buyer']){
-	       echo '<strong>Seller</strong>: ' .$result['seller'];
-	       $other = $result['seller'];
-	     }
-	     echo '<br>';
-	     $ct_isbn = $result['isbn'];
-	     $ct_slice_data = mysql_query("SELECT * FROM bookslice1 WHERE isbn = '$ct_isbn'") or die('Error slice: ' . mysql_error());
-	   
-	     while($result_inner = mysql_fetch_array($ct_slice_data)){
-	       echo '<strong>Title</strong>: ' . $result_inner['title'];
-	     }
-	     echo '<br>';
-	     $ct_sale_data = mysql_query("SELECT * FROM books_for_sale WHERE isbn = '$ct_isbn' AND (seller = '$user' OR seller = '$other')") or die('Error: ' . mysql_error());
-	      while($result_inner = mysql_fetch_array($ct_sale_data)){
-	        echo '<strong>Asking Price</strong>: ' . $result_inner['asking_price'];
-	      }
-	     echo '</div>';
-	   }
-	   echo '</div>';
+            $currtran_data = mysql_query("SELECT * FROM current_transactions WHERE seller = '$user' OR buyer = '$user'") or die("CURRTRAN: Error: " . mysql_error());
+            
+            while ($result = mysql_fetch_array($currtran_data)) {
+                echo '<input type="radio" name="pass_valCT" value="' . $result['isbn'] . '">';
+                echo '<div class="bookEntry">';
+                echo '<strong>Trade Date</strong>: ' . $result['tradedate'];
+                echo '<br>';
+
+                if ($_SESSION['username'] == $result['seller']) {
+                    echo '<strong>Buyer</strong>: ' . $result['buyer'];
+                    $other = $result['buyer'];
+                } else if ($_SESSION['username'] == $result['buyer']) {
+                    echo '<strong>Seller</strong>: ' . $result['seller'];
+                    $other = $result['seller'];
+                }
+                echo '<br>';
+
+                $ct_isbn = $result['isbn'];
+                $ct_slice_data = mysql_query("SELECT * FROM bookslice1 WHERE isbn = '$ct_isbn'") or die('Error slice: ' . mysql_error());
+
+                while ($result_inner = mysql_fetch_array($ct_slice_data)) {
+                    echo '<strong>Title</strong>: ' . $result_inner['title'];
+                }
+                echo '<br>';
+                
+                $ct_sale_data = mysql_query("SELECT * FROM books_for_sale WHERE isbn = '$ct_isbn' AND (seller = '$user' OR seller = '$other')") or die('Error: ' . mysql_error());
+                
+                while ($result_inner = mysql_fetch_array($ct_sale_data)) {
+                    echo '<strong>Asking Price</strong>: ' . $result_inner['asking_price'];
+                }
+                echo '</div>';
+            }
+            echo '</div>';
+            echo '</form>';
         }
         ?>
-    </form>
-</body>
+
+    </body>
 </html> 
